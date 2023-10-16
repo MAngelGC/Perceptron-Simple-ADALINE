d6e67aa3303797043e0a580fa63f0e88d85a0fb4
MAngelGC <magc6102@uma.es>
2023-10-16 15:42:28 +0200

    updated ADALINAAnalitico.m

--- ADALINAAnalitico.m
+++ ADALINAAnalitico.m
@@ -12,31 +12,15 @@ load DatosLS10
 Data(:,end)=Data(:,end)==1;
 
 Limites=[-1.5, 2.5, -1.5, 2.5];
-MaxEpoc=100;
 
 
 W = PerceptronWeigthsGenerator(Data);
 Y = Data(:,end);
 
-Epoc=1;
-
-%while ~CheckPattern(Data,W) && Epoc<MaxEpoc
-        % GrapDatos(Data,Limites);
-        % GrapPatron(Input,Output,Limites);
-        % GrapNeuron(W,Limites);hold off;
-        % drawnow
-        % pause;
-        
-        X = [Data(:,1:end-1) -ones(length(Data), 1)];
-        W = inv(X'*X)*X'*Y;
-    
-        % GrapDatos(Data,Limites);
-        % GrapPatron(Input,Output,Limites)
-        % GrapNeuron(W,Limites);hold off;
-        % drawnow
-        % pause;
-        %Epoc=Epoc+1;
-%end
+       
+X = [Data(:,1:end-1) -ones(length(Data), 1)];
+W = inv(X'*X)*X'*Y;
+   
 
 acc = sum(Y == max(Signo(X*W), 0))/length(Y);
 acc
